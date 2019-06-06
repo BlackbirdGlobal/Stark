b9a107ce11d07be06ad2d1b77e64b10fda8941aa
ArtD <artem.drozdoff@gmail.com>
2019-06-07 01:04:55 +0300

    Binary Tree II test program

--- Stark/Program.cs
+++ Stark/Program.cs
@@ -13,32 +13,12 @@ namespace Stark
     {
         static void Main(string[] args)
         {
-            var d = new TreeDictionary<string,string>( new BinaryTree<string, string>());
-            d.Add("Foo","Bar");
-            d.Add("Marko","Polo");
+            var d = new TreeDictionary<string, string>(new BinaryTree<string, string>())
+            {
+                {"Foo", "Bar"}, {"Marko", "Polo"}
+            };
             d.TryGetValue("Marko", out string r);
             Console.WriteLine(r);
-        }
-
-        private static string[] _matrix;
-
-        private static string CountLuck(int k)
-        {
-            var builder = new StringMatrixGraphBuilder();
-            var graph = builder.Build(_matrix);
-            IGraphSearcher<char, int> s = new BFSSearcher<char, int>(graph);
-            var r = s.Search(builder.Destination, (n, v) =>
-            {
-                if (n.Parent != null)
-                    n.AdditionalData = n.Parent.AdditionalData;
-                if (n.Value == v)
-                    return;
-                if (n.Children != null && n.Children.Count > 1)
-                    n.AdditionalData += 1;
-            });
-            if (!r.Found)
-                return "Oops!";
-            return r.Result.AdditionalData == k ? "Impressed" : "Oops!";
         }
     }
 }
