0b634d122bda7c96182b3a1827877ba182c25a9c
Artem Drozdov <artem_drozdov@epam.com>
2019-06-13 18:06:53 +0300

    tree tests

---
+++ Stark.UnitTests/BinaryTreeDictionaryTests.cs
@@ -0,0 +1,38 @@
+﻿using Stark.Collections;
+using Stark.Trees;
+using Xunit;
+
+namespace Stark.UnitTests
+{
+    public class BinaryTreeDictionaryTests
+    {
+        [Fact]
+        public void ContainsKey_InsertNull_ReturnsTrue()
+        {
+            //Arrange
+            var dictionary = new TreeDictionary<string, string>(new BinaryTree<string, string>());
+            const string key = "testKey";
+            dictionary.Add(key, null);
+            //Act
+            var result = dictionary.ContainsKey(key);
+            //Assert
+            Assert.True(result);
+            Assert.Single(dictionary);
+        }
+
+        [Fact]
+        public void ContainsKey_InsertNotNull_ReturnsTrue()
+        {
+            //Arrange
+            var dictionary = new TreeDictionary<string, string>(new BinaryTree<string, string>());
+            const string key = "testKey";
+            const string val = "test";
+            dictionary.Add(key, val);
+            //Act
+            var result = dictionary.ContainsKey(key);
+            //Assert
+            Assert.True(result);
+            Assert.Single(dictionary);
+        }
+    }
+}
