bd117e416d9c220c77bcd9a903c181b004afeb15
Artem Drozdov <Artem_Drozdov@epam.com>
2020-02-27 11:38:42 +0200

    string extensions mkII

---
+++ Blackbird.Stark.Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Blackbird.Stark.Collections;
+using Blackbird.Stark.Extensions;
+using Blackbird.Stark.Trees;
+
+namespace Blackbird.Stark.App
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            var number = Console.ReadLine();
+            var r = number.IsNumber();
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ Blackbird.Stark/Extensions/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Blackbird.Stark.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly char[] Numbers = new []{'0','1','2','3','4','5','6','7','8','9'};
+        
+        public static bool IsNumber(this string self)
+        {
+            if (self.IsNullOrWhiteSpace())
+                return false;
+            var trimmed = self.Trim();
+            var hasDelimiter = false;
+            for (var i = 0; i < trimmed.Length; i++)
+            {
+                if (i == 0 && trimmed[i] == '-')
+                    continue;
+                if (trimmed[i] == '.' || trimmed[i] == ',')
+                {
+                    hasDelimiter = true;
+                    continue;
+                }
+                if (hasDelimiter && (trimmed[i] == '.' || trimmed[i] == ','))
+                {
+                    return false;
+                }
+
+                if (!Numbers.Contains(trimmed[i]))
+                    return false;
+            }
+            return true;
+        }
+        
+        public static bool IsNullOrWhiteSpace(this string self)
+        {
+            return string.IsNullOrWhiteSpace(self);
+        }
+        
+        public static bool IsNullOrEmpty(this string self)
+        {
+            return string.IsNullOrEmpty(self);
+        }
+    }
+}
