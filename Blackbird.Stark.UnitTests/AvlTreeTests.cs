bd5a2b7d72df2c73dafaced74d4bd8e6e7d81017
Artem Drozdov <artem.drozdoff@gmail.com>
2020-06-14 01:09:05 +0300

    geeks for geeks avl tree test

--- Blackbird.Stark.UnitTests/AvlTreeTests.cs
+++ Blackbird.Stark.UnitTests/AvlTreeTests.cs
@@ -22,5 +22,27 @@ namespace Blackbird.Stark.UnitTests
             //Assert
             Assert.NotEqual("A",result.Value);
         }
+        [Fact]
+        public void GeeksForGeeks_AvlTest()
+        {
+            //Arrange
+            var tree = new AvlTree<int,string>();
+            tree.Add(10,"A");
+            tree.Add(20,"B");
+            tree.Add(30,"C");
+            tree.Add(40,"D");
+            tree.Add(50,"E");
+            tree.Add(25,"F");
+            //Act
+            var result = tree._root;
+            //Assert
+            Assert.NotEqual("A",result.Value);
+            Assert.Equal("C", result.Value);
+            Assert.Equal("D", result.Right.Value);
+            Assert.Equal("E", result.Right.Right.Value);
+            Assert.Equal("B", result.Left.Value);
+            Assert.Equal("A", result.Left.Left.Value);
+            Assert.Equal("F", result.Left.Right.Value);
+        }
     }
 }
