cdf2ee82f7c38859e549d1a7841a8074ccb6211a
Drozdov <artem.drozdoff@gmail.com>
2022-01-06 00:06:32 +0200

    rb-tree initial commit

---
+++ Blackbird.Stark/Trees/Nodes/RbNode.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Blackbird.Stark.Trees.Nodes;
+
+public enum RbColor
+{
+    Red,
+    Black
+}
+
+public class RbNode<TK,TV> where TK:IComparable<TK>
+{
+    public TK Key { get; set; }
+    public TV Value { get; set; }
+    public RbColor Color { get; set; }
+    public RbNode<TK, TV> Left { get; set; }
+    public RbNode<TK, TV> Right { get; set; }
+
+    public RbNode(TK key, TV value)
+    {
+        Key = key;
+        Value = value;
+        Color = RbColor.Black;
+    }
+}
